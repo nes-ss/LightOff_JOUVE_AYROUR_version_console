d8c52534bb5897871344caea17815c056070d75e
ayrou <ayrou@10.3.61.116>
2024-11-29 15:22:11 +0100

    Création d'un nouveau projet versionné

--- src/lightoff_jouve_ayrour_version_console/GrilleDeCellules.java
+++ src/lightoff_jouve_ayrour_version_console/GrilleDeCellules.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package lightoff_jouve_ayrour_version_console;
 
 /**
  *
  * @author ayrou
  */
-public class LightOff {
+public class GrilleDeCellules {
     
 }
