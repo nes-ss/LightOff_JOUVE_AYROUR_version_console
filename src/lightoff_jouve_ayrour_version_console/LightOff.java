5de940149213035934a2b319f8c56ae994f2e3c7
Ambre Jouve <ambre.jouve@orange.frepfedu.fr>
2024-12-04 11:34:48 +0100

    Création d'un nouveau projet versionné

---
+++ src/lightoff_jouve_ayrour_version_console/LightOff.java
@@ -0,0 +1,19 @@
+package lightoff_jouve_ayrour_version_console;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ayrou
+ */
+public class LightOff {
+     public static void main(String[] args) {
+        Partie partie1 = new Partie();
+        partie1.initialiserPartie();
+        partie1.lancerPartie();
+        
+    }
+}
